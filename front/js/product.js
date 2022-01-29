7761da85fae734c7dd86545720d151a245f42ab3
AceSc35 <baptiste.lef@hotmail.fr>
2022-01-29 12:28:02 +0100

    step 6-7 done

--- front/js/product.js
+++ front/js/product.js
@@ -1,13 +1,108 @@
+chooseArticle();
+
 //Récupérer les paramètres de l'URL
 
-function settingURL (){
-
-    let url = new URL(window.location.href)
-    let search_params = new URLSearchParams(url.search)
-    if(search_params.has('id')) {
-        let id = search_params.get('id');
-        console.log(id)
-    }
+function settingURL() {
+  const url = new URL(window.location.href);
+  const search_params = new URLSearchParams(url.search);
+  const productID = search_params.get('id');
+  return productID;
 }
 
-settingURL();
+//Récupérer le produit sur la page en fonction de l'id
+
+function chooseArticle() {
+  const productID = settingURL();
+
+  fetch(`http://localhost:3000/api/products/${productID}`)
+    .then((response) => response.json())
+    .then((product) => {
+      const title = document.querySelector('#title');
+      const description = document.querySelector('#description');
+      const price = document.querySelector('#price');
+
+      //Création des balises HTML/API - Image
+
+      const productIMG = document.createElement('img');
+      document.querySelector('.item__img ').appendChild(productIMG);
+      productIMG.src = product.imageUrl;
+      productIMG.alt = product.altTxt;
+
+      //Création des balises HTML/API - Title-Description-Price
+
+      title.innerHTML = product.name;
+      price.innerHTML = product.price;
+      description.innerHTML = product.description;
+
+      // choix de la couleur
+
+      for (colors of product.colors) {
+        const productColor = document.createElement('option');
+        document.querySelector('#colors').appendChild(productColor);
+        productColor.value = colors;
+        productColor.innerHTML = colors;
+      }
+    });
+}
+//Rajouter Promise !!!
+
+function addToBasket() {
+  //Ajouter une quantité et l'ajouter dans le panier avec l'eventListener et local storage
+
+  const addToCart = document.querySelector('#addToCart');
+  const addQuantity = document.querySelector('#quantity');
+
+  addToCart.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    if (
+      addQuantity.value > 0 &&
+      addQuantity.value <= 100 &&
+      addQuantity.value != 0
+    ) {
+      let idBasket = 0;
+      let product = {
+        //Assignation des valeurs du produit dans le "cart"
+        idBasket: idBasket,
+        productID: settingURL(),
+        name: title.innerHTML,
+        quantity: document.querySelector('#quantity').value,
+        colors: document.querySelector('#colors').value,
+        price: price.innerHTML,
+      };
+      //Stocker la récupération des valeurs dans le local storage
+
+      //Déclaration de la variable 'productLocalStorage" dans laquelle on met les keys et valeurs qui sont dans le local storage
+      let productLocalStorage = JSON.parse(localStorage.getItem('cart'));
+
+      if (productLocalStorage) {
+        //S'il y a exactement le même produit avec la même couleur dans le local storage, on ajoute la quantité
+        const isProductInTheCart = productLocalStorage.find(
+          (productInCart) =>
+            productInCart.productID === product.productID &&
+            productInCart.colors === product.colors
+        );
+
+        // Ajoute la quantité d'un produit déjà existant dans le panier
+        if (isProductInTheCart) {
+          // Modifies la quantité et la couleur d'un produit déjà dans le panier avec sa nouvelle valeur
+          let newQuantity =
+            parseInt(isProductInTheCart.quantity) + parseInt(product.quantity);
+          isProductInTheCart.quantity = newQuantity;
+          localStorage.setItem('cart', JSON.stringify(productLocalStorage));
+        } else {
+          // Si le produit n'est pas dans le panier + panier existant => ajouter le produit dans le panier
+          productLocalStorage.push(product);
+          localStorage.setItem('cart', JSON.stringify(productLocalStorage));
+        }
+      } else {
+        // Si le panier est vide, ajouter le produit dans le panier
+        productLocalStorage = [];
+        productLocalStorage.push(product);
+        localStorage.setItem('cart', JSON.stringify(productLocalStorage));
+      }
+    }
+  });
+}
+
+addToBasket();
